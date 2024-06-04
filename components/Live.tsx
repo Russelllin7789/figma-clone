758b2d3bba012980c5e2f04ead2c6c5974aa569d
Russell Lin <superbr7789@gmail.com>
2024-06-04 14:48:54 +0800

    fulfill the live cursor function

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,11 +1,42 @@
-import { useOthers } from "@/liveblocks.config";
+import { useMyPresence, useOthers } from "@/liveblocks.config";
 import LiveCursors from "./cursor/LiveCursors";
+import { useCallback } from "react";
 
 const Live = () => {
   const others = useOthers();
+  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+  // return a memoized version of the callback
+  const handlePointerMove = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+
+    // to get the width of the actual cursor
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
+  const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+    updateMyPresence({ cursor: null, message: null });
+  }, []);
+
+  const handlePointerDown = useCallback((event: React.PointerEvent) => {
+    // to get the width of the actual cursor
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
 
   return (
-    <div>
+    <div
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+      onPointerDown={handlePointerDown}
+      className="h-[100vh] w-full flex justify-center items-center text-center"
+    >
+      <h1 className="text-2xl text-white">Liveblocks Figma Clone</h1>
       <LiveCursors others={others} />
     </div>
   );
