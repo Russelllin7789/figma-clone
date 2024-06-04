6c6c1e83de7b4d0d8ef962b66750accef9be0866
Russell Lin <superbr7789@gmail.com>
2024-06-04 12:27:13 +0800

    delete the collaborativaApp room component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
 
 export default function Page() {
   return (
     <Room>
-      <CollaborativeApp />
+      <h1>Testing~</h1>
     </Room>
   );
 }
