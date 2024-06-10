3b6d8f5804643f272fc21aef9d371fdef0b38ef4
Russell Lin <superbr7789@gmail.com>
2024-06-10 19:43:06 +0800

    resolve issues of liveblock comments

--- components/Live.tsx
+++ components/Live.tsx
@@ -11,6 +11,7 @@ import { CursorMode, CursorState, Reaction, ReactionEvent } from "@/types/type";
 import ReactionSelector from "./reaction/ReactionButton";
 import FlyingReaction from "./reaction/FlyingReaction";
 import useInterval from "@/hooks/useInterval";
+import { Comments } from "./comments/Comments";
 
 type Props = {
   canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
@@ -162,7 +163,7 @@ const Live = ({ canvasRef }: Props) => {
       onPointerLeave={handlePointerLeave}
       onPointerDown={handlePointerDown}
       onPointerUp={handlePointerUp}
-      className="h-[100vh] w-full flex justify-center items-center text-center"
+      className="relative h-full w-full flex flex-1 justify-center items-center"
     >
       <canvas ref={canvasRef} />
 
@@ -194,6 +195,8 @@ const Live = ({ canvasRef }: Props) => {
       )}
 
       <LiveCursors others={others} />
+
+      {/* <Comments /> */}
     </div>
   );
 };
